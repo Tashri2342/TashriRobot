c4a8cd6c437f20628c9e5903fb268e81181c8951
Tashri2342 <108603052+Tashri2342@users.noreply.github.com>
2023-07-09 16:16:38 +0530

    Update eval.py

--- TashriRobot/modules/eval.py
+++ TashriRobot/modules/eval.py
@@ -134,7 +134,7 @@ dispatcher.add_handler(EVAL_HANDLER)
 dispatcher.add_handler(EXEC_HANDLER)
 dispatcher.add_handler(CLEAR_HANDLER)
 
-__mod_name__ = "♨️ᴇᴠᴀʟ♨️"
+__mod_name__ = "⚡ᴇᴠᴀʟ⚡"
 __help__ = """
 ★ᴏᴡɴᴇʀ ᴄᴍᴅ ★
 ★ /eval :- to evaluate simple code
