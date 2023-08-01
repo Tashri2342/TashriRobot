cd46114bb0446a8454b5fe38bf34b82c7beea3f8
Tashri2342 <108603052+Tashri2342@users.noreply.github.com>
2023-08-01 21:28:31 +0530

    Update sexy.py

--- TashriRobot/modules/sexy.py
+++ TashriRobot/modules/sexy.py
@@ -4,7 +4,7 @@ from telethon import Button, events
 
 from .. import telethn as asst
 
-BUTTON = [[Button.url("🍒 ꜱᴜᴘᴘᴏʀᴛ 🍒", "https://t.me/TeamTashri")]]
+BUTTON = [[Button.url("🍒 ꜱᴜᴘᴘᴏʀᴛ 🍒", "https://t.me/TashriChatting")]]
 HOT = "https://telegra.ph/file/daad931db960ea40c0fca.gif"
 SMEXY = "https://telegra.ph/file/a23e9fd851fb6bc771686.gif"
 LEZBIAN = "https://telegra.ph/file/5609b87f0bd461fc36acb.gif"
